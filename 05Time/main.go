aa718ae01a557c0feea845f9fccf0cfda7443b2d
itsanindyak <iankoley04@gmail.com>
2024-10-24 19:16:19 +0530

    Add Go module and main file to display current time

---
+++ 05Time/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+	
+	fmt.Println(presentTime.Format("2006-01-02 15:04:05"))
+
+	
+}
