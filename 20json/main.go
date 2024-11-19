dc70ebce974bb494af3f7259f0e5fe7296c87c40
itsanindyak <iankoley04@gmail.com>
2024-11-19 23:32:10 +0530

    Add JSON decoding functionality and example data to main.go

--- 20json/main.go
+++ 20json/main.go
@@ -9,13 +9,23 @@ type course struct {
 	Name     string `json:"Course Name"`
 	Price    int    `json:"Course Price"`
 	Platform string
-	Password string   `json:"-"` // "-" ---> not include the field when send data
-	Tags     []string `json:"tags,omitempty"`  // "omitempty" ---> not include the field if that is nil
+	Password string   `json:"-"`              // "-" ---> not include the field when send data
+	Tags     []string `json:"tags,omitempty"` // "omitempty" ---> not include the field if that is nil
 }
+
+var jsonDataFromWeb = []byte(`
+	{
+        "Course Name": "React",
+        "Course Price": 299,
+        "Platform": "Youtube",
+        "tags": ["JS","Web-dev"]
+    }
+`)
 
 func main() {
 	encodeJSON()
-
+	decodeJSON(jsonDataFromWeb)
+	
 }
 
 func encodeJSON() {
@@ -30,4 +40,28 @@ func encodeJSON() {
 	}
 
 	fmt.Println(string(json))
+}
+
+func decodeJSON(jsonData []byte)  {
+	// var decodejson course
+
+	// isValid := json.Valid(jsonData)
+
+	// if isValid {
+	// 	json.Unmarshal(jsonData, &decodejson)
+	// }
+
+
+
+	// add data  to key value
+
+	var decodejson map[string]interface{}
+
+	json.Unmarshal(jsonData,&decodejson)
+
+
+	for key,value := range decodejson{
+
+		fmt.Println(key,"=",value)
+	}
 }
